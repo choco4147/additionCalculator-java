91e99f80b1020aa2f7c12308c0a769bc5242a0a9
Owen Murtha <100542480+choco4147@users.noreply.github.com>
2022-10-17 09:59:23 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,25 +2,25 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         // instantiating variables
-        Scanner scan = new Scanner(System.in);
-        String input = new String();
-        int answer = 0;
+        Scanner scan = new Scanner(System.in); // scanner, used to get input from console
+        String input = new String(); // will contain user input from console
+        int answer = 0; // contains final answer
         // say the instructions
         sayInstructions();
-        input = scan.nextLine();
-        while (!input.contains("=")) {
-            if (input.contains("help")) {
+        input = scan.nextLine(); 
+        while (!input.contains("=")) { // program stops scanning and prints answer if user inputs "="
+            if (input.contains("help")) { // prints the "help" text 
                 needHelp();
             }
-            else if (isNumeric(input)) {
+            else if (isNumeric(input)) { // adds numeric input to the final answer
                 answer = answer + Integer.parseInt(input);
             }
-            else {
+            else { // tells user to input a number 
                 System.out.println ("Your input did not contain a valid value (number, help, =)");
             }
-            input = scan.nextLine();
+            input = scan.nextLine(); // gets next user input 
         }
-        System.out.print(answer);
+        System.out.print(answer); // prints final answer
     }
     public static void sayInstructions() {
         // prints instructions in the console
@@ -39,12 +39,12 @@ public class Main {
         System.out.println ("Only one number should be input at a time");
         System.out.println ("Press enter/return before typing the next number");
     }
-    public static boolean isNumeric (String input) { 
+    public static boolean isNumeric (String input) { // determines if an input is numeric or not
         try {  
-        Double.parseDouble(input);  
+        Double.parseDouble(input); // parse int, returns true if it is an int
         return true;
         } 
-        catch(NumberFormatException e) {  
+        catch(NumberFormatException e) {   // returns false if previous parseInt does not return true
         return false;  
         }  
     }
